79ffd549463eb905bf27cee14966e5cac9fa16bc
allem40306 <allem40306@gmail.com>
2018-12-02 21:02:01 +0800

    revise: dijkstra

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -1,20 +1,44 @@
-void dijkstra(int s){
-	//set vis[]=0 d[]=inf
-	priority_quque<Node>pq;
-	d[s]=0;
-	p[s]=s;
-	pq.push(Node(s,0));
-	while(!pq.empty()){
-		while(!pq.empty()&&vis[pq.top().p])pq.top();
-		if(pq.empty())break;
-		vis[pq.top().p]=1;
-		Node k=pq.top(); pq.pop();
-		for(auto t:v[k.p]){
-			if(d[k.p]+w[k.p][t]<d[t]){
-				d[t]=d[k.p]+w[k.p][t];
-				p[t]=k.p;
-				pq.push((Node){t,d[t]})
-			}
-		}
-	}
-}
+struct Dijkstra{
+    int n,m;
+    bool vis[N];
+    vector<edge>edges;
+    vector<int>g[N];
+    LL d[N];
+    int f[N];
+    void init(int nn){
+        n=nn;
+        for(int i=0;i<n;i++){
+            g[i].clear();
+        }
+        edges.clear();
+    }
+    void add_edge(int s,int t,LL d){
+        edges.push_back(edge(s,t,d));
+        g[s].push_back(edges.size()-1);
+    }
+    void dijkstra(int ss){
+        priority_queue<heap>Q;
+        for(int i=0;i<n;i++){
+            d[i]=INF;
+        }
+        d[ss]=0;
+        f[ss]=-1;
+        memset(vis,0,sizeof(vis));
+        Q.push(heap(0,ss));
+        heap x;
+        while(!Q.empty()){
+            x=Q.top(); Q.pop();
+            int p=x.p;
+            if(vis[p])continue;
+            vis[p]=1;
+            for(int i=0;i<g[p].size();i++){
+                edge& e=edges[g[p][i]];
+                if(d[e.t]>d[p]+e.d){
+                    d[e.t]=d[p]+e.d;
+                    f[e.t]=g[p][i];
+                    Q.push(heap(d[e.t],e.t));
+                }
+            }
+        }
+    }
+}d1,d2;
