45762d5f8483363a0ea432cf379fa461f5f502c0
allem40306 <allem40306@gmail.com>
2019-07-08 17:09:10 +0800

    revise LCA

--- Graph/LCA.cpp
+++ Graph/LCA.cpp
@@ -1,8 +1,38 @@
-void preprocess() {
-	for (int i = 1; i <= 25; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (par[j][i - 1] == -1 || par[par[j][i - 1]][i - 1] == -1)continue;
-			par[j][i] = par[par[j][i - 1]][i - 1];
-		}
-	}
+const int LOG = 20;
+int par[N][LOG];
+int tin[N], tout[N];
+int timer = 0;
+
+void dfs(int v, int p){
+    tin[v] = ++timer;
+    par[v][0] = p;
+    for(int it: G[v]){
+        if(it != p)dfs(it, v);
+    }
+    tout[v] = ++timer;
+}
+
+void Doubling(){
+    for(int i = 1; i < N; ++i){
+        for(int j = 1; j < LOG; ++j){
+            par[i][j] = par[par[i][j - 1]][j - 1];
+        }
+    }
+}
+
+bool anc(int v, int u){
+    return tin[v] <= tin[u] && tout[u] <= tout[v]; 
+}
+
+int LCA(int v, int u){
+    if(anc(v, u))return v;
+    for(int j = LOG - 1; j >= 0; --j){
+        if(!anc(par[v][j], u))v = par[v][j];
+    }
+    return par[v][0];
+}
+
+int main(){
+    dfs(root, root);
+    Doubling();
 }
