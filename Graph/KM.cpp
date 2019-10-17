56136336f78140cf9c4ce9f23a5b84cae8fac5e4
allem40306 <allem40306@gmail.com>
2019-10-17 22:39:15 +0800

    revise KM

--- Graph/KM.cpp
+++ Graph/KM.cpp
@@ -1,66 +1,85 @@
-int n;
-int Left[N];
-double w[N][N], Lx[N], Ly[N];
-bitset<N> vx, vy;
-
-bool match(int i)
+template <typename T>
+struct KM
 {
-    vx[i] = true;
-    for (int j = 1; j <= n; j++)
+    int n;
+    int Left[N];
+    T w[N][N], Lx[N], Ly[N];
+    bitset<N> vx, vy;
+    
+    void init(int _n)
     {
-        if ((fabs(Lx[i] + Ly[j] - w[i][j]) < 1e-9) && !vy[j])
-        {
-            vy[j] = true;
-            if (!Left[j] || match(Left[j]))
-            {
-                Left[j] = i;
-                return true;
-            }
-        }
+        n = _n;
     }
-    return false;
-}
 
-void update()
-{
-    double a = 1e30;
-    for (int i = 1; i <= n; i++)
+    bool match(int i)
     {
-        if (vx[i])
-            for (int j = 1; j <= n; j++)
-            {
-                if (!vy[j])
-                    a = min(a, Lx[i] + Ly[j] - w[i][j]);
-            }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (vx[i])
-            Lx[i] -= a;
-        if (vy[i])
-            Ly[i] += a;
-    }
-}
-
-void KM()
-{
-    for (int i = 1; i <= n; i++)
-    {
-        Left[i] = Lx[i] = Ly[i] = 0;
+        vx[i] = true;
         for (int j = 1; j <= n; j++)
         {
-            Lx[i] = max(Lx[i], w[i][j]);
+            if ((fabs(Lx[i] + Ly[j] - w[i][j]) < 1e-9) && !vy[j])
+            {
+                vy[j] = 1;
+                if (!Left[j] || match(Left[j]))
+                {
+                    Left[j] = i;
+                    return true;
+                }
+            }
         }
+        return false;
     }
-    for (int i = 1; i <= n; i++)
+
+    void update()
     {
-        while (true)
+        T a = 1e9;
+        for (int i = 1; i <= n; i++)
         {
-            vx.reset();
-            vy.reset();
-            if (match(i))
-                break;
-            update();
+            if (vx[i])
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (!vy[j])
+                    {
+                        a = min(a, Lx[i] + Ly[j] - w[i][j]);
+                    }
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (vx[i])
+            {
+                Lx[i] -= a;
+            }
+            if (vy[i])
+            {
+                Ly[i] += a;
+            }
         }
     }
-}
+
+    void hungarian()
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Left[i] = Lx[i] = Ly[i] = 0;
+            for (int j = 1; j <= n; j++)
+            {
+                Lx[i] = max(Lx[i], w[i][j]);
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            while (1)
+            {
+                vx.reset();
+                vy.reset();
+                if (match(i))
+                {
+                    break;
+                }
+                update();
+            }
+        }
+    }
+};
