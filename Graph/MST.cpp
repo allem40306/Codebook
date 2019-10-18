bde4ebe11a728b441a66d7dff8724c319a235cc6
allem40306 <allem40306@gmail.com>
2019-10-18 10:49:50 +0800

    revise MST

--- Graph/MST.cpp
+++ Graph/MST.cpp
@@ -1,145 +1,23 @@
-#include <algorithm>
-#include <cstring>
-#include <iostream>
-#include <stack>
-#include <vector>
-
-#define LL long long
-#define MAX 1e11
-#define S 50050
-using namespace std;
-
-int n, m;
-int sum;
-
-typedef struct
+int MST()
 {
-    int a, b, l;
-} edge;
-bool cmp(edge l, edge r) { return l.l < r.l; }
-
-vector<edge> v;
-
-typedef struct
-{
-    int d;
-    LL l;
-} node;
-
-vector<node> map[S];
-
-int disjoint[S];
-
-int root(int x)
-{
-    if (disjoint[x] < 0)
-        return x;
-    else
-    {
-        disjoint[x] = root(disjoint[x]);
-        return disjoint[x];
-    }
-}
-
-bool same(int a, int b) { return root(a) == root(b); }
-
-void connect(int a, int b)
-{
-    // cout << "CONNECT " << a << " " << b << endl;
-    int ra = root(a);
-    int rb = root(b);
-
-    disjoint[ra] += disjoint[rb];
-    disjoint[rb] = ra;
-}
-
-void kruskal()
-{
-    int remain = n - 1;
-    for (auto i : v)
-    {
-        if (remain == 0)
-            break;
-
-        if (!same(i.a, i.b))
-        {
-            connect(i.a, i.b);
-
-            map[i.a].push_back((node){i.b, i.l});
-            map[i.b].push_back((node){i.a, i.l});
-
-            sum += i.l;
-            remain--;
-        }
-    }
-}
-
-bool book[S];
-
-void dfs(int start)
-{
-    stack<int> st;
-    st.push(start);
-
-    memset(book, false, sizeof(book));
-
-    while (!st.empty())
-    {
-        int cur = st.top();
-        // cout << cur << endl;
-        st.pop();
-
-        book[cur] = true;
-
-        for (int i = 0; i < map[cur].size(); i++)
-        {
-            int next = map[cur][i].d;
-            if (!book[next])
-            {
-                st.push(next);
-            }
-        }
-    }
-}
-
-void init()
-{
-    memset(disjoint, -1, sizeof(disjoint));
-    sum = 0;
-}
-
-bool check()
-{
-    for (int i = 1; i <= n; i++)
-        if (!book[i])
-            return false;
-
-    return true;
-}
-
-int main()
-{
-    init();
-
+    DisjointSet djs;
+    vector<Edge> edges;
+    int n, m, ans = 0;
     cin >> n >> m;
-
-    for (int i = 0; i < m; i++)
+    for (int i = 0, from, to, w; i < m; i++)
     {
-        edge tmp;
-        cin >> tmp.a >> tmp.b >> tmp.l;
-
-        v.push_back(tmp);
+        cin >> from >> to >> w;
+        edges.push_back({from, to, w});
     }
-
-    sort(v.begin(), v.end(), cmp);
-
-    kruskal();
-    dfs(1);
-
-    if (!check())
-        cout << -1 << endl;
-    else
-        cout << sum << endl;
-
-    return 0;
+    sort(edges.begin(), edges.end());
+    djs.init(n);
+    for (auto edge : edges)
+    {
+        // Union also check if to vertex haven't connected   
+        if (djs.Union(edge.from, edge.to))
+        {
+            ans += edge.w;
+        }
+    }
+    return ans;
 }
