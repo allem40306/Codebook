00aa253996414e39522908de28a159c0799228dd
allem40306 <allem40306@gmail.com>
2020-02-02 22:37:53 +0800

    update on 2020-02-02 22:37

--- Basic/Template.h
+++ Basic/Template.h
@@ -1,9 +1,18 @@
 #pragma GCC optimize(2)
 #include <bits/stdc++.h>
 using namespace std;
+using LL = long long;
 using PII = pair<int,int>;
+using PLL = pair<LL, LL>;
 const int INF = 1e9;
+const int MXN = 0;
+const int MXV = 0;
 #define MP  make_pair
+#define PB push_back
+#define F first
+#define S second
+#define FOR(i, L, R) for(int i = L; i != (int)R; ++i)
+#define FORD(i, L, R) for(int i = L; i != (int)R; --i)
 #define IOS cin.tie(nullptr); cout.tie(nullptr); ios_base::sync_with_stdio(false);
 
 int main()
